d03ff8673c78795e6b4129062d604f1d801e013a
kamiazya <yuki@kamiazya.tech>
2023-06-23 00:31:40 +0900

    first commit

---
+++ packages/pluggable-io/lib/index.js
@@ -0,0 +1,4 @@
+console.log('Hello');
+export function hello(name) {
+    console.log('Hello', name);
+}
---
+++ packages/pluggable-io/src/index.ts
@@ -0,0 +1,5 @@
+console.log('Hello')
+
+export function hello(name: string) {
+  console.log('Hello', name)
+}
