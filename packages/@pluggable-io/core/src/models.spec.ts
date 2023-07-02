269273383402ccd182bf5f07eacf8e87170b8f05
kamiazya <yuki@kamiazya.tech>
2023-07-02 19:22:23 +0900

    add test

---
+++ packages/@pluggable-io/core/src/models.spec.ts
@@ -0,0 +1,41 @@
+import { beforeEach, describe, expect, it } from 'vitest';
+import { RegistoryBase } from './models.js';
+import { PluginNotInstalled } from './errors.js';
+
+describe('RegistoryBase', () => {
+  let registory: RegistoryBase<{
+    test: string;
+  }>;
+  beforeEach(async () => {
+    registory = new RegistoryBase();
+  })
+  describe('registerPlugin function', () => {
+    it('should register a plugin', () => {
+      const plugin = {
+        build: async () => ({
+          test: 'test',
+        }),
+      };
+      registory.registerPlugin('test:', plugin)
+      expect(registory.plugins.get('test:')).toBe(plugin);
+    })
+  })
+  describe('from function', () => {
+    it('should return an instance of plugin', async () => {
+      const plugin = {
+        build: async () => ({
+          test: 'test',
+        }),
+      };
+      registory.registerPlugin('test:', plugin)
+      const instance = await registory.from('test://')
+      expect(instance).toStrictEqual({
+        test: 'test',
+      });
+    })
+
+    it('should throw an error if no plugin is registered', async () => {
+      await expect(registory.from('not-registerd://')).rejects.toThrow(PluginNotInstalled)
+    })
+  })
+})
--- packages/@pluggable-io/core/src/models.ts
+++ packages/@pluggable-io/core/src/models.ts
@@ -7,16 +7,16 @@ import { Registory, ResourcePlugin } from "./types.js";
  * @todo Add caching mechanism for instances
  */
 export class RegistoryBase<T> implements Registory<T> {
-  #plugins = new Map<string, ResourcePlugin<T>>();
+  plugins = new Map<string, ResourcePlugin<T>>();
 
   registerPlugin(protocol: string, plugin: ResourcePlugin<T>) {
-    this.#plugins.set(protocol, plugin);
+    this.plugins.set(protocol, plugin);
   }
 
-  from(url: string): Promise<T> {
+  async from(url: string): Promise<T> {
     const url_ = new URL(url);
-    const plugin = this.#plugins.get(url_.protocol);
-    if (!plugin) throw new PluginNotInstalled(`No plugin registered for protocol ${url_.protocol}`)
+    const plugin = this.plugins.get(url_.protocol);
+    if (!plugin) throw new PluginNotInstalled(`No plugin registered for protocol "${url_.protocol}"`)
     return plugin.build(url_);
   }
 }
