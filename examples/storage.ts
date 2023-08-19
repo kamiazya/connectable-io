4c5eee5cb9bb7aa1d312d4c0a7e8238af78dd8e3
kamiazya <yuki@kamiazya.tech>
2023-08-19 15:52:20 +0900

    update storage example

--- examples/storage.ts
+++ examples/storage.ts
@@ -1,5 +1,62 @@
+/**
+ * Create a file and write to it, or read a file and write it to another file.
+ */
 import { Storage } from 'pluggable-io'
-const storage = await Storage.from('file://.')
 
-const files = await storage.list()
-console.log(`Got ${files.length} files`, { files })
+try {
+  const storage = await Storage.from('file://tmp')
+
+  if ((await storage.exists('.gitignore')) === false) {
+    // Create ,gitignore if it doesn't exist
+    const file = await storage.open('.gitignore')
+    const stream = await file.createWriteStream()
+    const writer = stream.getWriter()
+    try {
+      await writer.write('*')
+      console.log('Created .gitignore')
+    } catch (e) {
+      console.error('Failed to write to .gitignore', e)
+    } finally {
+      console.log('Closing .gitignore')
+      writer.close()
+    }
+  } else {
+    console.log('.gitignore already exists')
+  }
+
+  const files = await storage.list()
+  console.log(`Got ${files.length} files`, files)
+
+  if (!files.includes('input.txt')) {
+    // Create a file and write to it
+    console.log('Creating input.txt')
+    const file = await storage.open('input.txt')
+    const stream = await file.createWriteStream()
+    const writer = stream.getWriter()
+    try {
+      await writer.write('Hello World!')
+      console.log('Wrote to input.txt')
+    } catch (e) {
+      console.error('Failed to write to input.txt', e)
+    } finally {
+      console.log('Closing input.txt')
+      writer.close()
+    }
+  } else {
+    // Read a file and write it to another file
+    const input = await storage.open('input.txt')
+    const output = await storage.open('output.txt')
+
+    const inputContent = await input.createReadStream()
+
+    const outputContent = await output.createWriteStream()
+    console.log('Copying input.txt to output.txt')
+    await inputContent.pipeTo(outputContent)
+    console.log('Copied input.txt to output.txt')
+  }
+
+  const filesAfter = await storage.list()
+  console.log(`Got ${filesAfter.length} files`, filesAfter)
+} catch (e) {
+  console.error(e)
+}
