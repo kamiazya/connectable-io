8982b6c0bab7e4a76eaf47febb2c5533e4d58b11
kamiazya <yuki@kamiazya.tech>
2023-11-17 23:02:18 +0900

    chaos: add eslint disable comment

--- packages/@connectable-io/storage-plugin-memory/src/plugins/MemoryStoragePlugin.ts
+++ packages/@connectable-io/storage-plugin-memory/src/plugins/MemoryStoragePlugin.ts
@@ -2,6 +2,7 @@ import { ResourcePlugin } from '@connectable-io/core'
 import { MemoryStorageAdapter } from '../adapters/MemoryStorageAdapter.js'
 
 export class MemoryStoragePlugin implements ResourcePlugin<MemoryStorageAdapter> {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async build(_url: string) {
     return new MemoryStorageAdapter()
   }
