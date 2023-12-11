2f253cd158be102c444c94a59da2d30fb482eda1
kamiazya <yuki@kamiazya.tech>
2023-12-11 22:08:15 +0900

    chaos: fix typo

--- packages/@connectable-io/storage-plugin-file/src/utils/get-glob-base.spec.ts
+++ packages/@connectable-io/storage-plugin-file/src/utils/get-glob-base.spec.ts
@@ -3,13 +3,13 @@ import { getGlobBase } from './get-glob-base.js'
 
 describe('getGlobBase', () => {
   test.each([
-    { pattrn: 'foo/bar/*.js', expected: 'foo/bar/' },
-    { pattrn: 'foo/bar/**/*.js', expected: 'foo/bar/' },
-    { pattrn: 'foo/bar/**/baz/*.js', expected: 'foo/bar/' },
-    { pattrn: 'foo/bar/**/baz/**/*.js', expected: 'foo/bar/' },
-    { pattrn: 'foo/bar/foo*.js', expected: 'foo/bar/' },
-    { pattrn: 'foo/bar/foo[0-9].js', expected: 'foo/bar/' },
-  ])('should return the base directory "$expected" for the given glob pattern "$pattrn"', ({ pattrn, expected }) => {
-    expect(getGlobBase(pattrn)).toBe(expected)
+    { pattern: 'foo/bar/*.js', expected: 'foo/bar/' },
+    { pattern: 'foo/bar/**/*.js', expected: 'foo/bar/' },
+    { pattern: 'foo/bar/**/baz/*.js', expected: 'foo/bar/' },
+    { pattern: 'foo/bar/**/baz/**/*.js', expected: 'foo/bar/' },
+    { pattern: 'foo/bar/foo*.js', expected: 'foo/bar/' },
+    { pattern: 'foo/bar/foo[0-9].js', expected: 'foo/bar/' },
+  ])('should return the base directory "$expected" for the given glob pattern "$pattern"', ({ pattern, expected }) => {
+    expect(getGlobBase(pattern)).toBe(expected)
   })
 })
